585ae7870349feb3d68e9cbe38e3dc48dbeb6110
Oleg833 <oleg.kovtun447@gmail.com>
2023-06-25 02:56:50 +0300

    work

--- index.js
+++ index.js
@@ -1,36 +1,50 @@
-// const { greet } = require("./helpers/greeting.js");
-// const { appendFile, readFile, writeFile } = require("fs/promises");
+// const main = () => {
+//   console.log("Konnichiwa Oleh!!!");
+// };
+// main();
+
+// const { greet } = require("./helpers/greeting");
 
 // greet();
 
-// const fs = require("fs/promises");
 // const filePath = "./data.txt";
+
+// const { appendFile, readFile, writeFile } = require("fs/promises");
+
+// const fs = require("fs/promises");
 
 // const fileOperation = async ({ action }) => {
 //   switch (action) {
 //     case "read":
-//       const res = await fs.readFile(filePath);
-//       console.log("read =", res.toString());
+//       const result = await fs.readFile(filePath, "utf-8");
+//       console.log("res=", result);
 //       break;
-//     case "rm":
-//       const result = await fs.writeFile(filePath, "\n New phrase");
-//       console.log("rm =", result);
+//     case "write":
+//       const append = await fs.appendFile(filePath, "\n from Tomato");
 //       break;
-//     case "add":
-//       const add = await fs.appendFile(filePath, "\n from Naruto");
-//       console.log("Add = ", add);
+//     case "replace":
+//       const replace = await fs.writeFile(filePath, "New phrase");
+//       console.log("repl=", replace);
 //       break;
-
+//     case "delete":
+//       const deleted = await fs.unlink(filePath);
+//       console.log(deleted);
+//       break;
 //     default:
-//       console.log("fault operation");
+//       console.log("Unknown action");
+//       console.warn("\x1B[31m Unknown action type!");
 //       break;
 //   }
 // };
-// fileOperation({ action: "rm" });
+// fileOperation({ action: "read" });
+// fileOperation({ action: "replace" });
+// fileOperation({ action: "delete" });
 
+// fileOperation({ action: "write" });
 // const { readMovie } = require("./movies/readMovies");
 
 // readMovie();
+
 // const path = require("path");
 // const filePath = path.resolve(__dirname, "db", "contacts.json");
 // const filePath = path.join(__dirname, "db", "contacts.json");
@@ -39,30 +53,35 @@
 // console.log("first = ", textDb);
 // console.log("join =", filePath);
 
+const argv = require("yargs").argv;
+
 const { getAll, getById, add, updateById, removeById } = require("./contacts");
 // getAll();
 const invokeAction = async ({ action, id, title, author }) => {
   switch (action) {
     case "getAll":
       const AllBooks = await getAll();
-      console.log(AllBooks);
+      console.table(AllBooks);
+      break;
+    case "list":
+      const list = await getAll();
+      console.table(list);
       break;
     case "getById":
       const oneBook = await getById(id);
-      console.log("id_start=", id);
-      console.log(oneBook);
+      console.table(oneBook);
       break;
     case "add":
       const newBook = await add({ title, author });
-      console.log(newBook);
+      console.table(newBook);
       break;
     case "updateById":
       const updatedBook = await updateById(id, { title, author });
-      console.log(updatedBook);
+      console.table(updatedBook);
       break;
     case "removeById":
       const removedBook = await removeById(id);
-      console.log(removedBook);
+      console.table(removedBook);
       break;
 
     default:
@@ -81,10 +100,12 @@ const invokeAction = async ({ action, id, title, author }) => {
 //   author: "Real McCoy",
 // });
 // invokeAction({ action: "removeById", id: "aoXlA5CAit9y1yQEAwNOR" });
-console.log(process.argv);
+// console.log(process.argv);
 
-const actionIndex = process.argv.indexOf("--action");
-if (actionIndex !== -1) {
-  const action = process.argv[actionIndex + 1];
-  invokeAction({ action });
-}
+// const actionIndex = process.argv.indexOf("--action");
+// if (actionIndex !== -1) {
+//   const action = process.argv[actionIndex + 1];
+//   invokeAction({ action });
+// }
+
+invokeAction(argv);
